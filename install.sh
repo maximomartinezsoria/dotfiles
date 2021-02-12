464f48255a492af69f9d79327f1f08a948f6b034
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-02-12 12:26:34 -0300

    Add mac deafults and zsh plugins

--- install.sh
+++ install.sh
@@ -4,18 +4,29 @@ brew update
 
 # Allow keep a key pressed on mac
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Install zsh and oh-my-zsh
 brew install zsh
 chsh -s /usr/local/bin/zsh
 compaudit | xargs chmod g-w,o-w
 
+# Install zsh theme
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp ./oh-my-zsh/mms.zsh-theme ~/.oh-my-zsh/themes
+
+# Install zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestionsgit
 
 # Install vim and nvim
 brew install vim nvim
 cp -r nvim/ ~/.config/nvim
+
+# Install Plug (Vim package manager)
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Update git and install github cli
 brew install git
@@ -51,5 +62,4 @@ composer global require laravel/valet
 valet install
 
 # Install Fira Code
-brew tap homebrew/cask-fonts
-brew cask install font-fira-code
+brew install font-fira-code
