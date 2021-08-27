932a22807ac76230cbaa28556d4ff0e77af9c471
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-08-27 10:49:25 -0300

    Export bin folder

--- shell/exports.sh
+++ shell/exports.sh
@@ -4,4 +4,4 @@ export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
 
 export PATH="$PATH:~/.composer/vendor/bin"
 export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
-export PATH="$DOTFILES_PATH/bin/dot:$PATH"
+export PATH="$DOTFILES_PATH/bin:$PATH"
