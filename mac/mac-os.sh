b4aadc33a30c0b7ce7054132825e1609e1f5b9a8
Maximo Martinez Soria <mmartinez@40q.agency>
2024-08-16 09:46:05 -0300

    Remove dock changes

--- mac/mac-os.sh
+++ mac/mac-os.sh
@@ -7,10 +7,3 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 defaults write NSGlobalDomain KeyRepeat -int 1         # Speed
 defaults write NSGlobalDomain InitialKeyRepeat -int 10 # Speed
 
-###############################################################################
-# 👨‍💻 Dock
-###############################################################################
-for _ in $(seq 1 4); do defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'; done # Add 4 spaces
-defaults write com.apple.dock autohide -bool true
-defaults write com.apple.dock show-recents -bool false
-killall Dock
