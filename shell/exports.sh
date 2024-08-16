200c2a1586cf2fae84fad4609039df754f3319d9
Maximo Martinez Soria <mmartinez@40q.agency>
2024-08-16 09:46:31 -0300

    Exports - update composer path

--- shell/exports.sh
+++ shell/exports.sh
@@ -2,6 +2,6 @@ export EDITOR='vim'
 export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
 
-export PATH="$PATH:~/.composer/vendor/bin"
+export PATH="$PATH:/Users/$(whoami)/.composer/vendor/bin"
 export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
 export PATH="$DOTFILES_PATH/bin:$PATH"
