b3d6bb6b82dc68fe224d14f5e4d34f0ab6785952
Maximo Martinez Soria <mmartinez@40q.agency>
2024-08-16 09:46:23 -0300

    Bootstrap - do not install valet

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
+DOTFILES_PATH=/Users/$(whoami)/dotfiles
 source "$DOTFILES_PATH/scripts/core/install.sh"
 
-install_brew
+# install_brew
 install_brew_apps
 
 set_zsh_default_shell
 
 install_zim
 install_nvm
-install_valet
 
 source "$DOTFILES_PATH/mac/mac-os.sh"
 source "$DOTFILES_PATH/symlinks.sh"
